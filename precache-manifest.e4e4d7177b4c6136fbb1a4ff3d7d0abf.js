ef666370d5ee02941b283cd6ac4c73619ba62a8e
mdjunaid14 <mdjunaid14@outlook.com>
2019-10-07 20:52:35 +0530

    initial commit

---
+++ precache-manifest.e4e4d7177b4c6136fbb1a4ff3d7d0abf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a12b217f74e4c0e82b727da208eb964",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c93faa7a604c77d5b70c",
+    "url": "/static/css/main.55071a35.chunk.css"
+  },
+  {
+    "revision": "fe20f9ce4a2e10f4a2f1",
+    "url": "/static/js/2.c3e0ea70.chunk.js"
+  },
+  {
+    "revision": "c93faa7a604c77d5b70c",
+    "url": "/static/js/main.2cb63330.chunk.js"
+  },
+  {
+    "revision": "09e27f03fdb94530568a",
+    "url": "/static/js/runtime-main.9d4f7790.js"
+  }
+]);
